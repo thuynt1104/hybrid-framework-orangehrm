c91ab6dd8ce6a50cf53dc1b57785276d8e261e50
thuynguyen <thuynt11494@gmail.com>
2025-05-20 15:51:31 +0700

    edit

--- testcases/com/orangehrm/admin/Employee_01.java
+++ testcases/com/orangehrm/admin/Employee_01.java
@@ -6,7 +6,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Login01 {
+public class Employee_01 {
     private WebDriver driver;
 
     @BeforeClass
